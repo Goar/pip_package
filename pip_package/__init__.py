4567a02f645e969999c254d340a0f6093f64a38a
Goar Orue Sanchez <goarorue@gmail.com.com>
2023-08-26 18:27:42 -0400

    updated setup.py and created my_cool_test_method

---
+++ pip_package/__init__.py
@@ -0,0 +1,2 @@
+def my_cool_test_method():
+    print('Goar: It works!')
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pip_package',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url=' https://github.com/Goar/pip_package',
+    author='Goar Orue Sanchez',
+    author_email='goaroue@gamil.com',
+    license='unlicense',
+    packages=['pip_package'],
+    zip_safe=False
+)
