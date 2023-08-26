17dba6e78683b81da26037d2b94362d760cfdb48
Goar Orue Sanchez <goarorue@gmail.com.com>
2023-08-26 19:27:41 -0400

    add a class

--- pip_package/__init__.py
+++ pip_package/__init__.py
@@ -1,2 +1,14 @@
 def my_cool_test_method():
     print('Goar: It works!')
+
+
+class GreatThing:
+    def __init__(self, datasource_config):
+        self.name = "Goar"
+        self.age = 46
+        
+    def print_age(self):
+        print(f"Age: {self.age}")
+
+    def print_name(self):
+        print(f"Name: {self.name}")
